11a690171d71ad4e0150349a1ad2ed40b4fb30ad
Jashan <jjscjashan03@gmail.com>
2022-05-19 23:33:28 +0530

    First commit

---
+++ Second/js/music-list.js
@@ -0,0 +1,50 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "Dill Lutiya",
+      artist: "Jazzy B",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Desire",
+      artist: "Ap Dillon",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Lover",
+      artist: "Diljit Dosanjh",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Spaceship",
+      artist: "Ap Dhillon",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Tere Te ",
+      artist: "Ap Dhillon",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+        name: "Most Wanted",
+        artist: "Ap Dhillon",
+        img: "music-6",
+        src: "music-6"
+      },
+
+  ];
